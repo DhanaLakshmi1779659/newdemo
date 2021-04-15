4206b373ab3909c53c28ce9d48f1ca2120b1a34f
DhanaLakshmi1779659 <81734112+DhanaLakshmi1779659@users.noreply.github.com>
2021-04-15 15:30:36 +0530

    Create exam.java
    basic

---
+++ exam.java
@@ -0,0 +1,8 @@
+public class trail
+{
+  public static void main(String Args[])
+  {
+     System.out.println("this is example build in jenkins");
+     System.out.println("this is update");
+  }
+}
