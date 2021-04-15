1b29e686fadcd48f381595d55c98ccc7f3732212
DhanaLakshmi1779659 <81734112+DhanaLakshmi1779659@users.noreply.github.com>
2021-04-15 15:37:41 +0530

    Update exam.java
    committed

--- exam.java
+++ exam.java
@@ -3,6 +3,6 @@ public class trail
   public static void main(String Args[])
   {
      System.out.println("this is example build in jenkins");
-     System.out.println("this is update");
+    
   }
 }
